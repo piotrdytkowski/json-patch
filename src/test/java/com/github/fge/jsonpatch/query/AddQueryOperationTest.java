57fd17f0977175d1f0eebd97637bc18ebc03d1ca
Piotr Dytkowski <piotr.dytkowski@gmail.com>
2022-01-26 09:59:14 +0100

    Add comments in not active tests

--- src/test/java/com/github/fge/jsonpatch/query/AddQueryOperationTest.java
+++ src/test/java/com/github/fge/jsonpatch/query/AddQueryOperationTest.java
@@ -3,7 +3,7 @@ package com.github.fge.jsonpatch.query;
 import com.github.fge.jsonpatch.JsonPatchOperationTest;
 
 import java.io.IOException;
-
+// TODO extend with JsonPatchOperationTest and uncomment constructor when this test needs to be active, couldn't ignore it otherway
 public class AddQueryOperationTest extends Object {
 
     public AddQueryOperationTest() throws IOException {
--- src/test/java/com/github/fge/jsonpatch/query/RemoveQueryOperationTest.java
+++ src/test/java/com/github/fge/jsonpatch/query/RemoveQueryOperationTest.java
@@ -4,6 +4,7 @@ import com.github.fge.jsonpatch.JsonPatchOperationTest;
 
 import java.io.IOException;
 
+// TODO extend with JsonPatchOperationTest and uncomment constructor when this test needs to be active, couldn't ignore it otherway
 public class RemoveQueryOperationTest extends Object {
 
     public RemoveQueryOperationTest() throws IOException {
