2ffa2c005493ef49b662b5aa47ee5c344641df9f
Piotr Dytkowski <piotr.dytkowski@gmail.com>
2022-01-26 18:01:24 +0100

    Extract regex constants

--- src/main/java/com/github/fge/jsonpatch/JsonPathParser.java
+++ src/main/java/com/github/fge/jsonpatch/JsonPathParser.java
@@ -2,13 +2,16 @@ package com.github.fge.jsonpatch;
 
 public class JsonPathParser {
 
+    private static final String ARRAY_ELEMENT_REGEX = "\\.(\\d+)\\.";
+    private static final String ARRAY_ELEMENT_LAST_REGEX = "\\.(\\d+)$";
+
     public static String tmfStringToJsonPath(String path) {
         if ("/".equals(path)) {
             return "$";
         }
         final String jsonPath = "$" + path.replace('/', '.')
-                .replaceAll("\\.(\\d+)\\.", ".[$1].")
-                .replaceAll("\\.(\\d+)$", ".[$1]");
+                .replaceAll(ARRAY_ELEMENT_REGEX, ".[$1].")
+                .replaceAll(ARRAY_ELEMENT_LAST_REGEX, ".[$1]");
         return jsonPath;
     }
 }
